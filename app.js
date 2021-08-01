7e7d3d4d77eb818d26326e1fb8212cbaf3cc2e12
Elena Sviridova <valeryk13@yandex.ru>
2021-08-01 09:29:46 +0300

    2nd commit

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const limitter = require('express-rate-limit');
+
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
+const userRoutes = require('./routes/user');
+const movieRoutes = require('./routes/movie');
+const auth = require('./middlewares/auth');
+// const NotFoundError = require('./errors/not-found-error');
+const { login, createUser } = require('./controllers/user');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+async function start() {
+  await mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+}
+
+start();
+
+app.use(requestLogger);
+app.use(limitter({
+  windowsMs: 5000,
+  max: 50,
+  message: {
+    code: 429,
+    message: 'Слишком много запросов, подождите',
+  },
+}));
+
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(4),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(4),
+    name: Joi.string().min(2).max(30),
+  }),
+}), createUser);
+
+app.use(auth);
+
+app.use('/users', userRoutes);
+
+app.use('/movies', movieRoutes);
+
+app.use((req, res, next) => {
+  try {
+    throw new Error('Запрашиваемый ресурс не найден');
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(errorLogger); // подключаем логгер ошибок
+
+app.use(errors());
+
+// app.use(centralisedErrorsHandler);
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,21 @@
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 
+const { JWT_SECRET = 'secret-key' } = process.env;
+
+
 module.exports = {
-  // login(req, res, next) {
-  //   const { email, password } = req.body;
-  //   User.findUserByCredentials(email, password)
-  //     .then((user) => {
-  //       const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
-  //       return res.send({ token });
-  //     })
-  //     .catch(next);
-  // },
+  login(req, res, next) {
+    const { email, password } = req.body;
+    User.findUserByCredentials(email, password)
+      .then((user) => {
+        const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+        return res.send({ token });
+      })
+      .catch(next);
+  },
   getUserInfo(req, res, next) {
     User.findById(req.user._id)
       .orFail(new Error('Пользователь с указанным _id не найден.'))
@@ -33,4 +36,28 @@ module.exports = {
       })
       .catch(next);
   },
+  createUser(req, res, next) {
+    const {
+      name, email, password,
+    } = req.body;
+    bcrypt.hash(password, 10)
+      .then((hash) => User.create({
+        name, email, password: hash,
+      }))
+      .then(() => res.status(200).send({
+        data: {
+          name, email
+        },
+      }))
+      .catch((err) => {
+        if (err.name === 'ValidationError') {
+          throw new Error('Переданы некорректные данные при создании пользователя');
+        } else if (err.name === 'MongoError') {
+          throw new Error('Ошибка базы данных');
+        } else {
+          next(err);
+        }
+      })
+      .catch(next);
+  }
 };

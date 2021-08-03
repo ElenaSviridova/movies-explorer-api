dc76a35fc0290dbdbcf57046c5df6ad997cff27f
Elena Sviridova <valeryk13@yandex.ru>
2021-08-03 22:42:10 +0300

    email unique

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ router.get('/me', getUserInfo);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    email: Joi.string().required().email(),
   }),
 }), updateProfile);
 
